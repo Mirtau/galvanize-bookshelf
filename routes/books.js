47773e5f580feead5c91c32610fb92e0f3f9b17c
Mirtau <tjj72@comcast.net>
2017-12-06 19:49:22 -0700

    redone seeds

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,48 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex.js')
 
-// YOUR CODE HERE
+router.get('/books', (req, res,next)=> {
+  knex('books')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
+  .orderBy('title', 'asc')
+  .then((book)=> {
+    res.status(200).send(book)
+    })
+})
+router.get('/books/:id', (req, res, next)=> {
+  knex('books')
+  .where('id' , req.params.id)
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
+  .first()
+  .then((book)=> {
+    res.status(200).send(book)
+    })
+})
+
+router.post('/books', (req, res, next)=> {
+  knex('books')
+  .insert({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl
+  }, '*')
+  .then(function(book) {
+    let data = {
+      id: book[0].id,
+      title: book[0].title,
+      author: book[0].author,
+      genre: book[0].genre,
+      description: book[0].description,
+      coverUrl: book[0].cover_url
+    }
+    res.send(data)
+  })
+  .done()
+})
+
 
 module.exports = router;
