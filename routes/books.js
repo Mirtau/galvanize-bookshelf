2f12ff0c6a049a4d3a9e8ce2af83fed37d2a3b59
Mirtau <tjj72@comcast.net>
2017-12-06 20:49:46 -0700

    patch working

--- routes/books.js
+++ routes/books.js
@@ -6,46 +6,67 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex.js')
 
-router.get('/books', (req, res,next)=> {
+router.get('/books', (req, res, next) => {
   knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
-  .orderBy('title', 'asc')
-  .then((book)=> {
-    res.status(200).send(book)
+    .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
+    .orderBy('title', 'asc')
+    .then((book) => {
+      res.status(200).send(book)
     })
 })
-router.get('/books/:id', (req, res, next)=> {
+router.get('/books/:id', (req, res, next) => {
   knex('books')
-  .where('id' , req.params.id)
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
-  .first()
-  .then((book)=> {
-    res.status(200).send(book)
+    .where('id', req.params.id)
+    .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
+    .first()
+    .then((book) => {
+      res.status(200).send(book)
     })
 })
 
-router.post('/books', (req, res, next)=> {
+router.post('/books', (req, res, next) => {
   knex('books')
-  .insert({
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    description: req.body.description,
-    cover_url: req.body.coverUrl
-  }, '*')
-  .then(function(book) {
-    let data = {
-      id: book[0].id,
-      title: book[0].title,
-      author: book[0].author,
-      genre: book[0].genre,
-      description: book[0].description,
-      coverUrl: book[0].cover_url
-    }
-    res.send(data)
-  })
-  .done()
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }, '*')
+    .then(function(book) {
+      let data = {
+        id: book[0].id,
+        title: book[0].title,
+        author: book[0].author,
+        genre: book[0].genre,
+        description: book[0].description,
+        coverUrl: book[0].cover_url
+      }
+      res.send(data)
+    })
+    .done()
 })
 
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .update(
+      {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    },
+    '*'
+  )
+    .then(data => {
+      data[0]['coverUrl'] = data[0]['cover_url']
+      delete data[0]['cover_url']
+      delete data[0]['created_at']
+      delete data[0]['updated_at']
+  res.status(200).send(data[0])
+})
+})
 
 module.exports = router;
